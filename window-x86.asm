5527198966abb9a468b576e8872237e26386157c
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-09-02 22:27:57 +0200

    Update the 32-bit code with more macro's

--- window-x86.asm
+++ window-x86.asm
@@ -5,15 +5,8 @@
 %include "libwindows-x86.inc"
 
 code_section
-    WindowProc:
-        push ebp
-        mov ebp, esp
-        %define hwnd dword [ebp + 2 * 4]
-        %define uMsg dword [ebp + 3 * 4]
-        %define wParam dword [ebp + 4 * 4]
-        %define lParam dword [ebp + 5 * 4]
-
-        mov eax, uMsg
+    function WindowProc, hwnd, uMsg, wParam, lParam
+        mov eax, [uMsg]
         cmp eax, WM_CREATE
         je .wm_create
         cmp eax, WM_GETMINMAXINFO
@@ -25,7 +18,14 @@ code_section
         jmp .default
 
         .wm_create:
-            invoke GetClientRect, hwnd, window_rect
+            local window_rect, RECT_size, \
+                new_window_width, 4, \
+                new_window_height, 4, \
+                new_window_x, 4, \
+                new_window_y, 4
+
+            lea eax, [window_rect]
+            invoke GetClientRect, [hwnd], eax
 
             mov eax, [window_width]
             shl eax, 1
@@ -37,85 +37,87 @@ code_section
             sub eax, [window_rect + RECT.bottom]
             mov [new_window_height], eax
 
-            push_reverse [new_window_width], [new_window_height], SWP_NOZORDER
+            invoke GetSystemMetrics, SM_CXSCREEN
+            sub eax, [new_window_width]
+            shr eax, 1
+            mov [new_window_x], eax
 
             invoke GetSystemMetrics, SM_CYSCREEN
             sub eax, [new_window_height]
             shr eax, 1
-            push eax
+            mov [new_window_y], eax
 
-            invoke GetSystemMetrics, SM_CXSCREEN
-            sub eax, [new_window_width]
-            shr eax, 1
-            push eax
-
-            invoke SetWindowPos, [ebp + 8], 0
+            invoke SetWindowPos, [hwnd], HWND_TOP, [new_window_x], [new_window_y], [new_window_width], [new_window_height], SWP_NOZORDER
 
             jmp .leave
 
+            %undef window_rect
+
         .wm_getminmaxinfo:
-            mov eax, lParam
+            mov eax, [lParam]
             mov dword [eax + MINMAXINFO.ptMinTrackSize + POINT.x], 320
             mov dword [eax + MINMAXINFO.ptMinTrackSize + POINT.y], 240
-
             jmp .leave
 
         .wm_paint:
-            invoke BeginPaint, hwnd, paint_struct
+            local paint_struct, PAINTSTRUCT_size, \
+                window_rect, RECT_size, \
+                hfont, 4
 
-            invoke GetClientRect, hwnd, window_rect
+            lea eax, [paint_struct]
+            invoke BeginPaint, [hwnd], eax
 
-            push_reverse 0, 0, 0, 400, 0, 0, 0, 0, 0, 0, CLEARTYPE_QUALITY, 0, font_name
+            lea eax, [window_rect]
+            invoke GetClientRect, [hwnd], eax
+
             mov eax, [window_rect + RECT.right]
             shr eax, 4
-            invoke CreateFontA, eax
+            invoke CreateFontA, eax, 0, 0, 0, 400, 0, 0, 0, 0, 0, 0, CLEARTYPE_QUALITY, 0, font_name
             mov [hfont], eax
 
             invoke SelectObject, [paint_struct + PAINTSTRUCT.hdc], [hfont]
             invoke SetBkMode, [paint_struct + PAINTSTRUCT.hdc], TRANSPARENT
             invoke SetTextColor, [paint_struct + PAINTSTRUCT.hdc], 0x00ffffff
-            invoke DrawTextA, [paint_struct + PAINTSTRUCT.hdc], window_title, -1, window_rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER
+
+            lea eax, [window_rect]
+            invoke DrawTextA, [paint_struct + PAINTSTRUCT.hdc], window_title, -1, eax, DT_SINGLELINE | DT_CENTER | DT_VCENTER
+
             invoke DeleteObject, [hfont]
 
-            invoke EndPaint, hwnd, paint_struct
+            lea eax, [paint_struct]
+            invoke EndPaint, [hwnd], eax
 
             jmp .leave
 
         .wm_destroy:
             invoke PostQuitMessage, 0
         .leave:
-            xor eax, eax
-            leave
-            ret 16
+            return 0
 
         .default:
-            invoke DefWindowProcA, hwnd, uMsg, wParam, lParam
-            leave
-            ret 16
+            invoke DefWindowProcA, [hwnd], [uMsg], [wParam], [lParam]
+            return
 
         %undef hwnd
 
-    rand:
-        push ebp
-        mov ebp, esp
-
-        mov eax, [seed]
-        imul eax, 1103515245
-
+    function rand
+        imul eax, [seed], 1103515245
         add eax, 12345
 
-        xor edx, edx
-        mov ebx, 1 << 31
-        idiv ebx
+        mov edx, 0
+        mov ecx, 1 << 31
+        idiv ecx
 
         mov [seed], edx
-        mov eax, edx
-
-        leave
-        ret
+        return edx
 
     _start:
-        invoke GetLocalTime, time
+        local time, SYSTEMTIME_size, \
+            hwnd, 4, \
+            message, MSG_size
+
+        lea eax, [time]
+        invoke GetLocalTime, eax
         mov eax, [time + SYSTEMTIME.wHour]
         imul eax, 60
         add eax, [time + SYSTEMTIME.wMinute]
@@ -129,25 +131,33 @@ code_section
         invoke LoadCursorA, 0, IDC_ARROW
         mov [window_class + WNDCLASSEX.hCursor], eax
 
-        call rand
-        shr eax, 8
+        stdcall rand
+        and eax, 0x00ffffff
         invoke CreateSolidBrush, eax
         mov [window_class + WNDCLASSEX.hbrBackground], eax
 
         invoke RegisterClassExA, window_class
 
         invoke CreateWindowExA, 0, window_class_name, window_title, WS_OVERLAPPEDWINDOW, \
-            CW_USEDEFAULT, CW_USEDEFAULT, [window_width], [window_height], HWND_DESKTOP, 0, [window_class + WNDCLASSEX.hInstance], 0
+            CW_USEDEFAULT, CW_USEDEFAULT, [window_width], [window_height], \
+            HWND_DESKTOP, 0, [window_class + WNDCLASSEX.hInstance], 0
         mov [hwnd], eax
         invoke ShowWindow, [hwnd], SW_SHOWDEFAULT
         invoke UpdateWindow, [hwnd]
 
         .message_loop:
-            invoke GetMessageA, message, 0, 0, 0
+            lea eax, [message]
+            invoke GetMessageA, eax, 0, 0, 0
+
             cmp eax, 0
             jle .done
-            invoke TranslateMessage, message
-            invoke DispatchMessageA, message
+
+            lea eax, [message]
+            invoke TranslateMessage, eax
+
+            lea eax, [message]
+            invoke DispatchMessageA, eax
+
             jmp .message_loop
 
         .done:
@@ -176,15 +186,6 @@ data_section
         dd 0                       ; lpszMenuName
         dd window_class_name       ; lpszClassName
         dd 0                       ; hIconSm
-
-    hwnd:
-    hfont:
-    new_window_width dd 0
-    new_window_height dd 0
-    time:
-    message times MSG_size db 0
-    window_rect times RECT_size db 0
-    paint_struct times PAINTSTRUCT_size db 0
 
     import_table
         library gdi_table, "GDI32.DLL", \
