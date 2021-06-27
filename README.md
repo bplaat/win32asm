cb61a117a97b19c2eae18f787d0546bc1ae3d0ee
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-06-27 11:14:54 +0200

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Pure 32-bit and 64-bit win32 assembly windows programs
 
 To assemble the programs you need the [NASM](https://nasm.us/) assembler. All headers are generated via macro's so you can compile on every platform and don't need specific Windows linkers. You could run the [`build.sh`](build.sh) script to assemble all the programs.
+
+In the folder `custom-gcc` is a little project which uses the gcc compiler to compile c code to assembley which then is assembled with custom headers via nasm. This gives you in essence the best of both worlds, but it is still experimental.
--- custom-gcc/redsquare/build.sh
+++ custom-gcc/redsquare/build.sh
@@ -6,7 +6,7 @@ nasm -f bin redsquare-x86.asm -o redsquare-x86.exe
 windres -UWIN64 redsquare.rc -o redsquare-x86.res
 ResourceHacker -open redsquare-x86.exe -save redsquare-x86.exe -action addskip -res redsquare-x86.res -log NUL
 
-if [[ $2 != "keep" ]]; then
+if [[ $1 != "keep" ]]; then
     rm redsquare-x86.s redsquare-x86.asm redsquare-x86.res
 fi
 
@@ -18,7 +18,7 @@ nasm -DWIN64 -f bin redsquare-x64.asm -o redsquare-x64.exe
 windres redsquare.rc -o redsquare-x64.res
 ResourceHacker -open redsquare-x64.exe -save redsquare-x64.exe -action addskip -res redsquare-x64.res -log NUL
 
-if [[ $2 != "keep" ]]; then
+if [[ $1 != "keep" ]]; then
     rm redsquare-x64.s redsquare-x64.asm redsquare-x64.res
 fi
 
