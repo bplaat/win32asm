1c2090b8dc29452a2cb01954de8fb85d16982f53
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-08-24 10:46:57 +0200

    Add http headers parsing to map

--- custom-gcc/examples/http/src/http.c
+++ custom-gcc/examples/http/src/http.c
@@ -2,21 +2,27 @@
 #include "hans.h"
 
 void _start(void) {
-    Uri *uri = Uri_New("http://www.example.com"); // http://www.plaatsoft.nl/android/coinlist/
+    Uri *uri = Uri_New("http://www.plaatsoft.nl/android/coinlist/"); // http://www.example.com
     printf("Uri scheme: %s\n", uri->scheme);
     printf("Uri host: %s\n", uri->host);
     printf("Uri port: %s\n", uri->port);
-    printf("Uri path: %s\n\n", uri->path);
+    printf("Uri path: %s\n", uri->path);
 
-    char *http_header;
-    char *response = Http_Get(uri, &http_header);
-    printf("Http header size: %d\n", (int32_t)strlen(http_header));
-    printf("Response size: %d\n\n", (int32_t)strlen(response));
-    puts(http_header);
-    puts(response);
-    free(http_header);
-
+    char *buffer;
+    Map *headers;
+    char *response = Http_Get(uri, &buffer, &headers);
     Uri_Free(uri);
+
+    printf("\n# Http headers:\n");
+    Map_Foreach(headers, char *key, char *value, {
+        printf("%s: %s\n", key, value);
+    })
+    Map_Free(headers, free);
+
+    printf("\n# Response (%d bytes):\n", (int32_t)strlen(response));
+    // puts(response);
+
+    free(buffer);
 
     ExitProcess(EXIT_SUCCESS);
 }
